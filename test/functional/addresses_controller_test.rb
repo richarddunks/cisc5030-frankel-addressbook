c3d518f25c9be04b78fc0951a5b721d203bc0dcd
Rick Frankel <rick@rickster.com>
2013-02-16 14:35:25 -0500

    fix address tests

--- test/functional/addresses_controller_test.rb
+++ test/functional/addresses_controller_test.rb
@@ -2,48 +2,54 @@ require 'test_helper'
 
 class AddressesControllerTest < ActionController::TestCase
   setup do
-    @address = addresses(:one)
+    @entry = entries(:valid)
+    @address = addresses(:valid)
   end
 
   test "should get index" do
-    get :index
+    get :index, entry_id: @entry
     assert_response :success
     assert_not_nil assigns(:addresses)
   end
 
   test "should get new" do
-    get :new
+    get :new, entry_id: @entry
     assert_response :success
   end
 
   test "should create address" do
     assert_difference('Address.count') do
-      post :create, address: { city: @address.city, state: @address.state, street: @address.street, zip: @address.zip }
+      post :create, entry_id: @entry, address: {
+        city: @address.city, state: @address.state,
+        street: @address.street, zip: @address.zip
+      }
     end
-
-    assert_redirected_to address_path(assigns(:address))
+    assert_redirected_to entry_address_path(@entry, assigns(:address))
   end
 
   test "should show address" do
-    get :show, id: @address
+    get :show, id: @address, entry_id: @entry
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @address
+    get :edit, id: @address, entry_id: @entry
     assert_response :success
   end
 
   test "should update address" do
-    put :update, id: @address, address: { city: @address.city, state: @address.state, street: @address.street, zip: @address.zip }
-    assert_redirected_to address_path(assigns(:address))
+    put :update, entry_id: @entry, id: @address, address: {
+      city: @address.city, state: @address.state, street: @address.street,
+      zip: @address.zip
+    }
+    assert_redirected_to entry_address_path(@entry, assigns(:address))
   end
 
   test "should destroy address" do
     assert_difference('Address.count', -1) do
-      delete :destroy, id: @address
+      delete :destroy, id: @address, entry_id: @entry
     end
 
-    assert_redirected_to addresses_path
+    assert_redirected_to entry_addresses_path(@entry);
   end
 end
